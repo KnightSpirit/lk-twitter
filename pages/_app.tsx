4c31fb4047f3c3e65d73d4396db2a9cdae82de22
limengze.shawn <limengze.shawn@bytedance.com>
2022-11-06 23:52:09 +0800

    fix: type mistake

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Layout from "components/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const getLayout = (Component as any).getLayout || ((page) => page);
+  const getLayout = (Component as any).getLayout || ((page: JSX.Element) => page);
   return getLayout(
     <Layout>
       <Component {...pageProps} />
